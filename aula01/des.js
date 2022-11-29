0a698f4447ecf61f1b52c6505c47b772b248154b
feliperuppel30 <115996060+feliperuppel30@users.noreply.github.com>
2022-11-29 11:22:04 -0300

    testezi n

---
+++ aula01/des.js
@@ -0,0 +1,13 @@
+//nota = 5
+//if(nota>=7){
+//    console.log(`parabens voce foi aprovado com a nota ${nota}`)
+//} else{
+//    console.log(`voce foi reprovado`)
+//}
+
+pizza = 9
+while(pizza > 0){
+    pizza--
+    console.log(`comi um pedaço de pizza, agora tenho ${pizza} fatias`)
+}
+console.log(`acabou a piza :(`)
