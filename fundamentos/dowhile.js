df843d1e9ea91cae3db2685b9a928458e9854b64
feliperuppel30 <115996060+feliperuppel30@users.noreply.github.com>
2022-12-01 11:15:58 -0300

    tst

--- fundamentos/dowhile.js
+++ fundamentos/dowhile.js
@@ -1,10 +1,12 @@
-function rand(min, max){
-    const valor = Math.random()*(max-min)+min
+
+function rand(min,max){
+    const valor = Math.random()*(max - min)+min
     return Math.floor(valor)
 }
-let opcao = 0
 
-while(opcao != -1){
-    opcao = rand(-1,10)
-    console.log(`a opcao escolhida foi ${opcao}`)
+let opcao = 0 
+
+while (opcao !=-1){
+    opcao = rand(-1, 10)
+    console.log(`opção escolhida foi ${opcao}`)
 }
